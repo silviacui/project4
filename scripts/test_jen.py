f0807beb951241654a9fd17da507591475d59278
silviacui <silviacui317@yeah.net>
2018-06-29 21:31:17 +0800

    上传忽略文件

---
+++ scripts/test_jen.py
@@ -0,0 +1,13 @@
+class TestJenkins:
+
+    def test_login1(self):
+        assert 1
+
+    def test_login2(self):
+        assert 1
+
+    def test_login3(self):
+        assert 1
+
+    def test_login4(self):
+        assert 1
