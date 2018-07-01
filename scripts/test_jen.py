b2a3bf90b19d5253d77649e4075db3fd9bd57b22
silviacui <silviacui317@yeah.net>
2018-07-01 12:10:15 +0800

    commit

--- scripts/test_jen.py
+++ scripts/test_jen.py
@@ -1,11 +1,12 @@
 class TestJenkins:
 
     def test_login1(self):
-        assert 0
+        assert 1
 
     def test_login2(self):
         assert 1
-       
+        print("123")
+
     def test_login3(self):
         assert 1
 
